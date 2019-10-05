99a2f13a5f14274931ae1229d21bf335b26263cd
jmmmnm <jmmmnm@gmail.com>
2019-10-05 23:56:39 +0900

    直接記述

---
+++ app/src/main/java/jp/techacademy/minami/jommo/ui/MainActivity.kt
@@ -0,0 +1,18 @@
+package jp.techacademy.minami.jommo.ui
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener {
+            // TextEditTextの文字列をTextViewに設定
+            textView.text = editText.text.toString()
+        }
+
+    }
+}
